985cf20db35f8413fbe6f33778db3b98aa6ae391
Danna Bohnhoff <dannab101@gmail.com>
2023-08-20 08:41:30 -0400

    add 404

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const express = require('express')
 const app = express()
 
 // Create a homepage route.
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     // This gets sent to the client
     // (your web browser most likely!)
-    res.send('Home Page')
+    res.send('Hello World!')
 })
 
-
+// 404 route
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Not Found</h1>')
+})
 
 // Listen for connections.
 // app.listen(3000, function () {
