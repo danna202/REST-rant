9f671fc5572c7bec64b62e1a6bf260530de2116a
Danna Bohnhoff <dannab101@gmail.com>
2023-08-27 10:53:56 -0400

    key index

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,42 +1,26 @@
 const React = require('react')
 const Def = require('../default')
 
-// function places () {
-//     return (
-//       <Def>
-//           <main>
-//               <h1>Places</h1>
-//           </main>
-//       </Def>
-//     )
-//   }
+
 function index (data) {
   let placesFormatted = data.places.map((place) => {
     return (
-      <div>
+      <div key={index}> 
         <h2>{place.name}</h2>
         <img src={place.pic} alt={place.name}/>
       </div>
-    )
-  })
+    );
+  });
+
   return (
     <Def>
         <main>
-            <h1>PLACES INDEX PAGE</h1>
+            <h1>PLACES TO RANT OR RAVE ABOUT</h1>
             {placesFormatted}
         </main>
     </Def>
 )
 }
-// function places () {
-//       return (
-//         <Def>
-//             <main>
-//                 <h1>Places</h1>
-//             </main>
-//         </Def>
-//       )
-//     }
-  
+
 
 module.exports = index
